9b95e927447f0e8b930f920db5305c71b72b6dad
Sam Q <saminm.qureshi@gmail.com>
2026-02-11 08:47:24 -0500

    Fix env variable name to MONGODB_URI

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
 });
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('MongoDB connection error:', err));
 
